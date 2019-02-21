06cf40b3c7b250691a89cad3b90dd408e2510694
VaishnuJM <32368681+VaishnuJM@users.noreply.github.com>
2019-02-21 15:55:53 +0530

    Update even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-   a =8,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
-  (a%2==0)
+   int a =8;
+  if(a%2==0)
   {
    printf("EVEN");
   }
@@ -10,7 +10,7 @@ int main()
   {
    printf("ODD");
   }
-  
+  return 0;
 }
    
   
